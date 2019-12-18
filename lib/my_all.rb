f6293df5016602c941f29caa503369682b5eb621
confident-token-1019 <theoonetj@gmail.com>
2019-12-18 06:45:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,12 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-i = 0 
-while i < collection. length 
-yield(collection[i])
-i = i + 1 
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
-end 
 
 binding.pry 
 my_all?([1,2,3]) {|i| i < 2}
